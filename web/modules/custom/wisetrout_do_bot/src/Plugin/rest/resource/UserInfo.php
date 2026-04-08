bcf7c1fd3ebd6b5f918c2f1a59884e93add395f2
MxLourier <maya.v.lourie@gmail.com>
2026-04-08 12:24:28 +0300

    working on userInfo endpoint

--- web/modules/custom/wisetrout_do_bot/src/Plugin/rest/resource/UserInfo.php
+++ web/modules/custom/wisetrout_do_bot/src/Plugin/rest/resource/UserInfo.php
@@ -4,6 +4,7 @@ namespace Drupal\wisetrout_do_bot\Plugin\rest\resource;
 
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
+use Drupal\node\Entity\Node;
 
 /**
  * Provides a resource to get Telegram bot user info.
@@ -18,33 +19,56 @@ use Drupal\rest\ResourceResponse;
  */
 class UserInfo extends ResourceBase {
 
-    public function get($cid){
+    private function readUserStatus($cid, $database){
+      $queryResult = $database
+      ->query("SELECT status FROM {telegram_subscribers} WHERE chat_id = :cid", [':cid' => $cid])
+      ->fetchAssoc();
+      $status = $queryResult ? $queryResult['status'] : null;
+      return $status;
+    }
 
-    $database = \Drupal::database();
-    $selectQuery = $database->query("SELECT module, status FROM {telegram_subscriptions} WHERE chat_id = :cid", [':cid' => $cid]);
-    $result = $selectQuery->fetchAll();
+    private function readModulesList($cid, $database){
 
-    $modules = [];
+      $dbRows = $database
+      ->query("SELECT module_id FROM {telegram_subscriptions} WHERE chat_id = :cid", [':cid' => $cid])
+      ->fetchAll();
 
-    if(count($result) === 0) {
-      $data = NULL;
-    } else {
+      $moduleIds = [];
+
+      foreach($dbRows as $dbRow){
+        $moduleIds[] = $dbRow->module_id;
+      }
+
+      $nodes = \Drupal\node\Entity\node::loadMultiple($nids);
 
       $modules = [];
 
-      foreach($result as $resultRow){
-        $modules[] = $resultRow->module;
+      foreach($nodes as $node){
+        $modules[] = $node->field_module_machine_name[0]->value;
       }
 
-      $data = [
-        'modules' => $modules,
-        'subscribed' => $result[0]->status == 1,
+      return $modules;
+
+      // return $moduleIds;
+    }
+
+    public function get($cid){
+
+    $database = \Drupal::database();
+    
+    $userStatus = $this->readUserStatus($cid, $database);
+    $responseData;
+
+    if($userStatus === null) {
+      $responseData = null;
+    }else {
+      $modulesList = $this->readModulesList($cid, $database);
+      $responseData = [
+        'subscribed' => !!$userStatus,
+        'modules' => $modulesList,
       ];
     }
 
-
-    return new ResourceResponse([
-      'userInfo' => $data,
-    ]);
+    return new ResourceResponse($responseData);
   }
 }
